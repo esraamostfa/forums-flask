2c81d20c5d16a0dc016a505b1141aaf6c161b6bc
Esraa Mostfa <esraamostfa13@gmail.com>
2018-03-04 10:26:34 +0200

    Fix bug

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,10 @@
-from app import stores
-from app import data
+from app import stores, models, data
 from flask import Flask
 
 app = Flask(__name__)
 
 members_store = stores.MembersStore()
 posts_store = stores.PostsStore()
+data.seed_stores(members_store, posts_store)
 
 from app.views import *
-
-if __name__  == "__main__":
-    data.seed_stores(members_store, posts_store)
-    app.run()
